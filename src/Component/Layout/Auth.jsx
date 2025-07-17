2552e4413f44f469df0c639b7b295b2455386842
unknown <nayemk295@gmail.com>
2025-07-17 21:21:41 +0600

    change log page img

--- src/Component/Layout/Auth.jsx
+++ src/Component/Layout/Auth.jsx
@@ -1,34 +1,28 @@
+
 import React from 'react';
 import { Outlet } from 'react-router';
-import logImg  from "../../assets/4957136.jpg"
+import logImg from "../../assets/login.webp";
+
 const Auth = () => {
-    return (
-        <div className='lg:flex   lg:w-7xl mx-auto'>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2">
 
-
-<div className="flex flex-col lg:flex-row lg:gap-4 p-10">
-     <div className="lg:flex-1 mt-12 lg:mt-0 flex justify-center">
-    
- <img
-      src={logImg}
-      alt="Logo"
-     
-      className="  max-sm:hidden    rounded-3xl  object-cover"
-    /></div>
- 
-
-
-  <div className="lg:flex-1">
-    <Outlet />
-  </div>
-
-
- 
-</div>
-
-
+        <div className="hidden md:flex items-center justify-center p-10">
+          <img
+            src={logImg}
+            alt="Logo"
+            className="rounded-3xl object-cover w-full max-w-md"
+          />
         </div>
-    );
+
+        {/* Right Form Side */}
+        <div className="flex items-center justify-center p-6 md:p-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
